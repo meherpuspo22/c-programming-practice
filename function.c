62b1188e01d5867cba8f19be3fd8b950880d3e07
meherpuspo22 <mehernigarpuspo03326@gmail.com>
2026-09-16 21:05:51 +0600

    add this basic function code

---
+++ function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// declaration / prototype
+void printHello();
+
+int main (){
+printHello();
+printHello();
+printHello();
+printHello();
+
+return 0;
+}
+
+// function definition
+void printHello(){
+    printf("Hello!\n");
+    printf("puspo");
+}
+
